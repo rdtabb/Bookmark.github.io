82b318e9f47a65629b807c0eab573ee0e6956c77
Rinat Tabaev <rdtabaev@edu.hse.ru>
2023-01-23 18:35:24 +0300

    solved some issues, added onscroll animations

---
+++ observers.js
@@ -0,0 +1,24 @@
+const faders = document.querySelectorAll('.disappear')
+
+const fadersOptions = { 
+    threshold: 0,
+    rootMargin: "0px 0px -150px 0px"
+}
+
+const appearOnScroll = new IntersectionObserver(function (
+    entries, 
+    appearOnScroll
+    ) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('slide-in')
+            } else {
+                entry.target.classList.remove('slide-in')
+            }
+        })
+    }, 
+fadersOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+})
